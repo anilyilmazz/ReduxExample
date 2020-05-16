5b7be880abcd7b3e943695844a895dbb6ae42e3b
anil yilmaz <anil.yilmaz@sys.com.tr>
2020-05-16 16:04:16 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
 import Counter from "./Counter";
+import { createStore } from "redux"
+
+const defaultState = {
+  count: 76,
+  name: "bob"
+}
+
+function reducer(state, action) {
+  console.log({ state, action })
+  if (action.type === "DECREASE") {
+    //  state.count = state.count -1 
+    //  return state
+    return {...state,count : state.count - action.payload}
+  }
+  return state
+}
+const store = createStore(reducer, defaultState);
+console.log(store.getState());
+
+store.dispatch({ type: "DECREASE",payload : 1 })
+
 const App = () => {
-  return <Counter />;
+  return <Counter state={store.getState()} />;
 };
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-function Counter() {
-  return <h1>counter component</h1>;
+function Counter(props) {
+  const amount = props.state.count;
+  const name = props.state.name;
+  const [count,setCount] = React.useState(1);
+  return (
+    <div className = "container">
+      <p>counter {amount}</p>
+      <p> {name}</p>
+
+      <div className = "buttons">
+        <button type ="button" className = "btn" 
+        onClick = {()=>setCount(count + 1) }>Inc</button>
+        <button type ="button" className = "btn" 
+        onClick = {()=>setCount(count -1) }>Dec</button>
+        <button type ="button" className = "btn" 
+        onClick = {()=>setCount(0) }>Rest</button>
+      </div>
+      
+    </div>
+  );
 }
 
 export default Counter;
