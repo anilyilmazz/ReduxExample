993ae5c7e4bd3a477fe0216031648ddeb3d6c061
anily <incelefon@gmail.com>
2020-05-16 17:26:43 +0300

    add chromeDevtools

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import Counter from "./Counter";
 import { createStore } from "redux"
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from "./reducers"
-import {Provider} from "react-redux"
+import { Provider } from "react-redux"
 
 const defaultState = {
   count: 0,
   name: "bob"
 }
-const store = createStore(reducer, defaultState);
-
+const store = createStore(reducer,defaultState, composeWithDevTools());
 const App = () => {
-  return( 
-  <Provider store = {store}>
-  <Counter />
-  </Provider>)
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>)
 };
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,26 +1,36 @@
 import React from "react";
-import {connect} from "react-redux"
-import {DECREASE,INCREASE,RESET} from "./actions"
-function Counter(props) {
+import { connect } from "react-redux"
+import { DECREASE, INCREASE, RESET } from "./actions"
+function Counter({ name, count, increase, decrease, reset }) {
   return (
-    <div className = "container">
-      <p>{props.name}</p>
-      <p>{props.count} </p>
- 
-      <div className = "buttons">
-        <button type ="button" className = "btn" 
-        onClick = {()=>props.dispatch({ type: INCREASE, payload: 1 }) }>Inc</button>
-        <button type ="button" className = "btn" 
-        onClick = {()=>props.dispatch({ type: DECREASE, payload: 1 }) }>Dec</button>
-        <button type ="button" className = "btn" 
-        onClick = {()=>props.dispatch({ type: RESET, payload: 1 }) }>Rest</button>
+    <div className="container">
+      <p>{name}</p>
+      <p>{count} </p>
+
+      <div className="buttons">
+        <button type="button" className="btn"
+          onClick={increase}>Inc</button>
+        <button type="button" className="btn"
+          onClick={decrease}>Dec</button>
+        <button type="button" className="btn"
+          onClick={reset}>Reset</button>
       </div>
-      
+
     </div>
   );
 }
+
 function mapStateToProps(state) {
-  return {count:state.count,name:state.name}
+  return { count: state.count, name: state.name }
+}
+function mapDispatchToProps(dispatch, ownProps) {
+  console.log(ownProps);
+  return {
+    increase: () => dispatch({ type: INCREASE, payload: 1 }),
+    decrease: () => dispatch({ type: DECREASE, payload: 1 }),
+    reset: () => dispatch({ type: RESET }),
+  }
+
 }
 
-export default connect(mapStateToProps) (Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
